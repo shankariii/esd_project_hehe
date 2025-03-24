86191dde7e4424230bab80f5731454c7e9ce48d6
Namyra Farouk <namyra.f.2023@scis.smu.edu.sg>
2025-03-24 12:16:07 +0800

    import flask_CORS for supplier and threshold microservice

--- my-coffeeshop-app/Backend/Threshold/threshold.py
+++ my-coffeeshop-app/Backend/Threshold/threshold.py
@@ -1,9 +1,12 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from os import environ
 
 app = Flask(__name__)
+CORS(app)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_recycle': 299}
