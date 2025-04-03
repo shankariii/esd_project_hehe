c0d23a90d92cf8913ca9513ff5091a287325de55
Bhobthefloofer <amritaaa725@gmail.com>
2025-04-03 13:00:05 +0800

    drink_ingredient_test

--- my-coffeeshop-app/Backend/drink_ingredients/drink_ingredients.py
+++ my-coffeeshop-app/Backend/drink_ingredients/drink_ingredients.py
@@ -1,25 +1,19 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from flask_restful import Api, Resource
-import pymysql
-import os
-from os import environ
 from flask_cors import CORS
-
+from os import environ
 
 # Initialize Flask App
 app = Flask(__name__)
-api = Api(app)
 CORS(app)
 
-# MySQL Configuration (Using Docker Environment Variables)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/drink_ingredients'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@host.docker.internal:3306/drink_ingredients'
+# MySQL Configuration using environment variable
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL')  
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_recycle': 299}
 
 # Initialize DB
 db = SQLAlchemy(app)
-
 
 # Define Drink Ingredients Model
 class DrinkIngredient(db.Model):
@@ -39,31 +33,37 @@ class DrinkIngredient(db.Model):
             "quantity": self.quantity,
             "unit": self.unit
         }
-    
 
 @app.route('/')
-def home():
-    return jsonify({"message": "Coffee Ordering System API is running!"}), 200
+def index():
+    return jsonify({"message": "Drink Ingredients microservice is running!"}), 200
 
+@app.route('/ingredients', methods=['GET'])
+@app.route('/ingredients/<int:drink_id>', methods=['GET'])
+def get_ingredients(drink_id=None):
+    """
+    Get all drink ingredients or filter by drink_id
+    ---
+    parameters:
+      - name: drink_id
+        in: path
+        type: integer
+        required: false
+        description: ID of the drink to filter ingredients
+    responses:
+      200:
+        description: List of ingredients
+      404:
+        description: No ingredients found
+    """
+    if drink_id:
+        ingredients = DrinkIngredient.query.filter_by(drink_id=drink_id).all()
+        if ingredients:
+            return jsonify([i.json() for i in ingredients]), 200
+        return jsonify({"message": "No ingredients found for this drink"}), 404
 
-# API Resources
-class DrinkIngredientResource(Resource):
-    def get(self, drink_id=None):
-        """Retrieve all drink ingredients or filter by drink_id"""
-        if drink_id:
-            ingredients = DrinkIngredient.query.filter_by(drink_id=drink_id).all()
-            if ingredients:
-                return jsonify([ing.json() for ing in ingredients])
-            return jsonify({"message": "No ingredients found for this drink"}), 404
-        
-        ingredients = DrinkIngredient.query.all()
-        return jsonify([ing.json() for ing in ingredients])
+    ingredients = DrinkIngredient.query.all()
+    return jsonify([i.json() for i in ingredients]), 200
 
-
-# Register API Endpoints
-api.add_resource(DrinkIngredientResource, '/ingredients', '/ingredients/<int:drink_id>')
-
-
-# Run App
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5006, debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5006)
