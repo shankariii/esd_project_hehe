11d16b06902d41049b8c29a9e5a488c8b77def11
Bhobthefloofer <amritaaa725@gmail.com>
2025-04-06 11:55:37 +0800

    update overall db

--- my-coffeeshop-app/SQL/coffeeShop_database.sql
+++ my-coffeeshop-app/SQL/coffeeShop_database.sql
@@ -174,15 +174,15 @@ values
 
 select * from customisation;
 -- drink_ingredients
-create schema drink_ingredients;
+DROP SCHEMA IF EXISTS drink_ingredients;
 
--- drop schema drink_ingredients;
+create schema drink_ingredients;
 
 use drink_ingredients;
 
 create table drink_ingredients
 (drink_ingredient_id int not null auto_increment,
-drink_id int not null ,
+drink_id int,
 ingredient varchar(255) not null,
 quantity int not null,
 unit varchar(15) not null,
@@ -190,8 +190,36 @@ constraint drink_ingredient_id_pk primary key (drink_ingredient_id));
 
 insert into drink_ingredients (drink_ingredient_id, drink_id, ingredient, quantity, unit)
 values 
-	(1, 1, "Caramel Syrup", 2, 'ounces'),
-	(2, 2, "Regular Milk", 100, 'ml');
+	-- Espresso (Drink ID 1)
+    (16, 1, 'Coffee Beans', 10, 'g'),
+
+    -- Latte (Drink ID 2)
+    (16, 2, 'Coffee Beans', 1, 'shot'),
+
+    -- Cappuccino (Drink ID 3)
+    (16, 3, 'Coffee Beans', 1, 'shot'),
+
+    -- Americano (Drink ID 4)
+	(16, 4, 'Coffee Beans', 10, 'g'),
+
+    -- Iced Matcha Latte (Drink ID 5)
+    (24, 5, 'Matcha Powder', 5, 'g'),
+	(20, 5, 'Sugar Syrup', 1, 'tbsp'),
+
+    -- Iced Chocolate (Drink ID 6)
+    (26, 6, 'Chocolate Syrup', 2, 'tbsp'),
+
+	-- no drink ids
+	(104, 'Regular Milk', 200, 'ml'),
+	(105, 'Skim Milk', 200, 'ml'),
+	(106, 'Almond Milk', 200, 'ml'),
+	(107, 'Oat Milk', 200, 'ml'),
+	(108, 'Soy Milk', 200, 'ml'),
+	(109, 'Vanilla Syrup', 2, 'tbsp'),
+	(110, 'Caramel Syrup', 2, 'tbsp'),
+	(111, 'Hazelnut Syrup', 2, 'tbsp'),
+	(112, 'Whipped Cream', 20, 'g'),
+	(113, 'Chocolate Sprinkles', 4, 'g');
     
 
 select * from drink_ingredients;
