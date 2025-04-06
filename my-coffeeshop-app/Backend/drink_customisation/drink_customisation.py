bcb5e6c7a21f2397af24e4d01e16dc7490374e69
Bhobthefloofer <amritaaa725@gmail.com>
2025-04-06 11:21:42 +0800

    drink_customisation_swagger

--- my-coffeeshop-app/Backend/drink_customisation/drink_customisation.py
+++ my-coffeeshop-app/Backend/drink_customisation/drink_customisation.py
@@ -1,14 +1,22 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flasgger import Swagger, swag_from
 from os import environ
 
 # Initialize Flask App
 app = Flask(__name__)
 CORS(app)
 
+# Flasgger config
+app.config['SWAGGER'] = {
+    'title': 'Drink Customisation Microservice API',
+    'uiversion': 3
+}
+swagger = Swagger(app)
+
 # MySQL Configuration using environment variable
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL')  
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_recycle': 299}
 
@@ -38,6 +46,36 @@ def home():
 
 @app.route('/customisations', methods=['GET'])
 @app.route('/customisations/<int:customisation_id>', methods=['GET'])
+@swag_from({
+    'tags': ['Drink Customisation'],
+    'parameters': [
+        {
+            'name': 'customisation_id',
+            'in': 'path',
+            'type': 'integer',
+            'required': False,
+            'description': 'ID of the customisation to retrieve'
+        }
+    ],
+    'responses': {
+        200: {
+            'description': 'All customisations or one by ID',
+            'examples': {
+                'application/json': [
+                    {
+                        "customisation_id": 1,
+                        "customisation_type": "milk",
+                        "name": "Oat Milk",
+                        "price_diff": 0.5
+                    }
+                ]
+            }
+        },
+        404: {
+            'description': 'Customisation not found'
+        }
+    }
+})
 def get_customisations(customisation_id=None):
     if customisation_id:
         customisation = Customisation.query.get(customisation_id)
@@ -49,6 +87,36 @@ def get_customisations(customisation_id=None):
     return jsonify([c.json() for c in customisations]), 200
 
 @app.route('/customisations/type/<string:customisation_type>', methods=['GET'])
+@swag_from({
+    'tags': ['Drink Customisation'],
+    'parameters': [
+        {
+            'name': 'customisation_type',
+            'in': 'path',
+            'type': 'string',
+            'required': True,
+            'description': 'Type of customisation (e.g., milk, size)'
+        }
+    ],
+    'responses': {
+        200: {
+            'description': 'Filtered customisations by type',
+            'examples': {
+                'application/json': [
+                    {
+                        "customisation_id": 2,
+                        "customisation_type": "size",
+                        "name": "Large",
+                        "price_diff": 1.0
+                    }
+                ]
+            }
+        },
+        404: {
+            'description': 'No customisations found for this type'
+        }
+    }
+})
 def get_customisations_by_type(customisation_type):
     customisations = Customisation.query.filter_by(customisation_type=customisation_type).all()
     if customisations:
