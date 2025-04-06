03fea2dd12165372ce024269d42492a2a2c88789
jxuanl <jiaxuanlai18@gmail.com>
2025-04-06 11:28:57 +0800

    Update drink_ingredients.sql

--- my-coffeeshop-app/SQL/drink_ingredients.sql
+++ my-coffeeshop-app/SQL/drink_ingredients.sql
@@ -14,12 +14,30 @@ constraint drink_ingredient_id_pk primary key (drink_ingredient_id));
 
 insert into drink_ingredients (drink_ingredient_id, drink_id, ingredient, quantity, unit)
 values 
-	(1, 1, "Caramel Syrup", 2, 'ounces'),
-	(2, 2, "Regular Milk", 200, 'ml'),
-	(3, 2, "Coffee Beans", 10, 'g'),
-	(4, 2, "Chocolate Sprinkles", 4, 'g'),
-	(5, 1, "Coffee Beans", 7, 'g');
+	-- Espresso (Drink ID 1)
+    (16, 1, 'Coffee Beans', 10, 'g'),
 
+    -- Latte (Drink ID 2)
+    (16, 2, 'Coffee Beans', 1, 'shot'),
+    (18, 2, 'Regular Milk', 200, 'ml'),
+
+    -- Cappuccino (Drink ID 3)
+    (16, 3, 'Coffee Beans', 1, 'shot'),
+    (18, 3, 'Regular Milk', 100, 'ml'),
+
+    -- Americano (Drink ID 4)
+	(16, 4, 'Coffee Beans', 10, 'g'),
+
+    -- Iced Matcha Latte (Drink ID 5)
+    (24, 5, 'Matcha Powder', 5, 'g'),
+	(20, 5, 'Sugar Syrup', 1, 'tbsp'),
+    (18, 5, 'Regular Milk', 180, 'ml'),
+
+    -- Iced Chocolate (Drink ID 6)
+    (26, 6, 'Chocolate Syrup', 2, 'tbsp'),
+    (18, 6, 'Regular Milk', 200, 'ml'),
+	(31, 2, "Chocolate Sprinkles", 4, 'g'),
+	(40, 6, 'Whipped Cream', 20, 'g');;
 
     
 
